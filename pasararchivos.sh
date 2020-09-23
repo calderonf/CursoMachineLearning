2a58187dd153ae86dafcde47dde072a816f03d49
Francisco Carlos Calderon <calderonf@gmail.com>
2020-09-23 11:57:17 -0500

    se suben PDFs de las presentaciones

---
+++ pasararchivos.sh
@@ -0,0 +1,16 @@
+cp Semana_1/Presentación2-definiciones.pdf CursoMachineLearning/Semana_1/
+
+cp Semana_2/probabilidad_nociones.pdf CursoMachineLearning/Semana_2
+
+cp Semana_3/Regresion_Lineal.pdf CursoMachineLearning/Semana_3
+cp Semana_3/TallerEnClase_1/Taller_en_clase_Intro_a_la_probabilidad_para_ML.ipynb CursoMachineLearning/Semana_3
+cp Semana_3/*.xlsx CursoMachineLearning/Semana_3
+
+cp Semana_4/*.pdf CursoMachineLearning/Semana_4
+cp Semana_4/*.xlsx CursoMachineLearning/Semana_4
+
+cp Semana_5/*.pdf CursoMachineLearning/Semana_5
+
+
+
+
